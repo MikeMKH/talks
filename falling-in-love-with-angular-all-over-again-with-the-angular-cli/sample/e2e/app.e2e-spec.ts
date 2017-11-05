7910b9625c972d4c8d24a585fae077d4c523e9d4
Mike Harris <MikeMKH@users.noreply.github.com>
2017-11-05 13:34:05 -0600

    Fixed issue with test failing

--- falling-in-love-with-angular-all-over-again-with-the-angular-cli/sample/e2e/app.e2e-spec.ts
+++ falling-in-love-with-angular-all-over-again-with-the-angular-cli/sample/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('demo-app App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to mke!');
+    expect(page.getParagraphText()).toEqual('Hello Milwaukee!');
   });
 });
