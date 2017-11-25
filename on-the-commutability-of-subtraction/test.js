c44facd62f3dc7bb9070d94d44f2ed50260375f0
Mike Harris <MikeMKH@users.noreply.github.com>
2017-11-25 15:37:46 -0600

    Updated code and slides

--- on-the-commutability-of-subtraction/test.js
+++ on-the-commutability-of-subtraction/test.js
@@ -1,9 +1,22 @@
 let assert = require('assert');
+let jsc = require('jsverify');
 
 describe('Subtraction', () => {
   describe('commutes', () => {
     it('0 - 0 === 0 - 0', () => {
       assert.deepEqual(0 - 0, 0 - 0);
     });
+    // jsc.property(
+    //   'forall a, b: int, a - b = b - a',
+    //   jsc.integer, jsc.integer,
+    //   (a, b) => a - b === b - a
+    // );
+  }),
+  describe('does not commute', () => {
+    jsc.property(
+      'forall a, b: int, a <> b -> a - b <> b - a',
+      jsc.integer, jsc.integer,
+      (a, b) => (a !== b) ? a - b !== b - a : true
+    );
   });
 });
