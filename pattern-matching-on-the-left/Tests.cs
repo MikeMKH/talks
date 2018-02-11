50114a08ffef1baec9950bab7f685ff8fb577722
Mike Harris <MikeMKH@users.noreply.github.com>
2018-02-11 15:46:22 -0600

    Added C# iterator example

---
+++ pattern-matching-on-the-left/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Tests
+{
+  public class Tests
+  {
+    [Fact]
+    public void GivenCollectionWeCanIteratorThroughIt()
+    {
+      IEnumerable<int> coll = new HashSet<int>() { 1, 2, 3 };
+      IEnumerator<int> iterator = coll.GetEnumerator();
+
+      var results = new LinkedList<int>();
+      while (iterator.MoveNext())
+      {
+        results.AddLast(iterator.Current);
+      }
+
+      Assert.Equal(results, coll);
+    }
+  }
+}
