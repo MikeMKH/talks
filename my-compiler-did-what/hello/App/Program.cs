14d8956377b2a526a6a13a9653b4a6d479bd0477
Mike Harris <MikeMKH@users.noreply.github.com>
2021-06-29 06:25:40 -0500

    add Hello

---
+++ my-compiler-did-what/hello/App/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conference = "That";
+            Console.WriteLine($"Hello {conference} Conference!");
+
+            Action<string> sorry =
+                conference => Console.WriteLine(
+                    $"Sorry, {conference} this is a bit ridiculous.");
+
+            sorry(conference);
+            Closing("fun");
+
+            static void Closing(string state)
+                => Console.WriteLine($"Hope you find it {state}!");
+        }
+    }
+}
