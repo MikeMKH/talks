671339c686b550f84fd6ff4d70922d642ff6efd9
Mike Harris <MikeMKH@users.noreply.github.com>
2021-06-30 06:19:48 -0500

    add top level statements

--- my-compiler-did-what/hello/App/Program.cs
+++ my-compiler-did-what/hello/App/Program.cs
@@ -1,23 +1,19 @@
 ﻿using System;
 
-namespace App
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var conference = "That";
-            Console.WriteLine($"Hello {conference} Conference!");
+var conference = "That";
+Console.WriteLine($"Hello {conference} Conference!");
 
-            Action<string> sorry =
-                conference => Console.WriteLine(
-                    $"Sorry, {conference} this is a bit ridiculous.");
+Action<string> sorry =
+  conference => Console.WriteLine(
+    $"Sorry, {conference} this is a bit ridiculous.");
 
-            sorry(conference);
-            Closing("fun");
+sorry(conference);
+Closing("fun");
 
-            static void Closing(string state)
-                => Console.WriteLine($"Hope you find it {state}!");
-        }
-    }
-}
+static void Closing(string state)
+  => Console.WriteLine($"Hope you find it {state}!");
+/*
+Hello That Conference!
+Sorry, That this is a bit ridiculous.
+Hope you find it fun!
+ */
