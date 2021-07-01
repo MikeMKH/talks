38a679009147b80641506cab5ab92b7123718cbb
Mike Harris <MikeMKH@users.noreply.github.com>
2021-07-01 06:21:32 -0500

    add IEnumerable

---
+++ my-compiler-did-what/enumerable/App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+foreach(var n in Fibonacci().Take(10))
+{
+    Console.Write($"{n}, ");
+}
+Console.WriteLine($"{Fibonacci().ElementAt(10)}");
+
+
+static IEnumerable<int> Fibonacci()
+{
+    yield return 0;
+
+    int value = 1;
+    int next = 1;
+    while (true)
+    {
+        yield return value;
+
+        int t = value;
+        value = next;
+        next += t;
+    }
+}
